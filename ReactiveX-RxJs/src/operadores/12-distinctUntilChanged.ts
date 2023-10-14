068c65a1ae09f814d418301776a98c6571d122ea
Daniel Perez Gonzalez <daniel.perez@banregio.com>
2023-10-13 19:27:16 -0600

    operador distinctUntilChanged – ReactiveX

---
+++ ReactiveX-RxJs/src/operadores/12-distinctUntilChanged.ts
@@ -0,0 +1,57 @@
+import { distinctUntilChanged, from, of } from "rxjs";
+
+const numeros$ = of(1, 2, 1, 3, 4, 4, 5, 6, 7, 2, 3, 1, 5, 44, '1', true, true);
+
+numeros$.pipe(
+  distinctUntilChanged()
+)
+.subscribe(console.log);
+
+interface Personaje {
+  tipo: string;
+  nombre: string;
+}
+
+const personajes: Personaje[] = [
+  {
+    tipo: 'heroe',
+    nombre: 'megaman'
+  },
+  {
+    tipo: 'heroe',
+    nombre: 'robin'
+  },
+  {
+    tipo: 'villano',
+    nombre: 'pingüino'
+  },
+  {
+    tipo: 'villano',
+    nombre: 'pingüino'
+  },
+  {
+    tipo: 'villano',
+    nombre: 'Thanos'
+  },
+  {
+    tipo: 'heroe',
+    nombre: 'Naruto'
+  },
+  {
+    tipo: 'villano',
+    nombre: 'Douma'
+  },
+  {
+    tipo: 'heroe',
+    nombre: 'Zenitsu'
+  },
+  {
+    tipo: 'villano',
+    nombre: 'pingüino'
+  },
+];
+
+from(personajes).pipe(
+  distinctUntilChanged((ant, act) => ant.nombre === act.nombre)
+)
+.subscribe(console.log)
